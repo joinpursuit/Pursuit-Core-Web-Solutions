bff63b46cf5f051453e4f8b404085663839ff1f6
Ben Stone <benjamin.h.stone@gmail.com>
2019-10-04 14:58:51 -0400

    add cors

--- express-routing/calculatorServer.js
+++ express-routing/calculatorServer.js
@@ -1,8 +1,11 @@
 const express = require('express')
+const cors = require('cors')
 
 const app = express()
+app.use(cores())
 
 app.get("/add/:numOne/:numTwo", (req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
   let n1 = parseFloat(req.params.firstNum)
   let n2 = parseFloat(req.params.secondNum)
   res.json({'result': n1 + n2})
--- express-routing/index.js
+++ express-routing/index.js
@@ -1,11 +1,10 @@
-const axios = require('axios')
-
 document.addEventListener('DOMContentLoaded', () => {
   let form = document.querySelector("#calculatorForm")
   form.addEventListener('submit', getResultAndUpdateUI)
 })
 
-const getResultAndUpdateUI = () => {
+async function getResultAndUpdateUI(event) {
+  event.preventDefault()
   let firstNum = document.querySelector('#numOne').value
   let secondNum = document.querySelector('#numTwo').value
   let response = await axios.get("http://localhost:8000/").data
